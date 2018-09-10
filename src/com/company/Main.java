00be105638bd27db265f345b200c12f331120009
Brain-drain <yuliagreat@mail.ru>
2018-09-10 11:00:47 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int  rez=1, n=10;
+        for (int i = 0; i < n; i++) {
+            rez*=2;
+
+        }
+        System.out.println( rez);
+
+    }
+}
